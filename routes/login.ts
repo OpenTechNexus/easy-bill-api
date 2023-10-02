19ffcd384db44a1273b93c2ac7095bf549537720
whoisjays <djince.d@gmail.com>
2023-10-02 17:34:36 +0300

    feat: EABAPI-1 added login route

---
+++ routes/login.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { Request, Response } from "express";
+
+const router = express.Router();
+
+router.post("/login", async (req: Request, res: Response) => {
+  return res.status(200).json("Hello world");
+});
+
+export default router;
