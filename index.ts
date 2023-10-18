a277ece6cc74f2ba02f96b9f043e046aff47386a
whoisjays <djince.d@gmail.com>
2023-10-18 17:18:10 +0300

    feat: EABAPI-9 added types

--- index.ts
+++ index.ts
@@ -11,8 +11,11 @@ app.use(express.json());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Access-Control-Allow-Origin', env.ENVIRONMENT);
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST, PATCH, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With, Content-Type, Accept, Authorization',
+  );
   next();
 });
 
